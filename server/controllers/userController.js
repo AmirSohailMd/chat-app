80c52d4b2ca78ef76e43055a079976b269c620bc
AmirSohailMd <amirsohailmd7@gmail.com>
2026-03-01 16:31:05 +0530

    Designed userController, userRoutes, userModel.Tested the registration API, valid response.Password hashing is working fine, JWT token generation is working fine

---
+++ server/controllers/userController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
+// @route   POST /api/users/register
+// @desc    Register new user
+// @access  Public
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser };
--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ const PORT = process.env.PORT || 5000;
 
 const express = require("express");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
 const app = express();
+
 app.use(express.json());
+app.use("/api/users", userRoutes);
+
 connectDB();
 
-app.get("/", (req, res)=> {res.send("API is running..");});
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
