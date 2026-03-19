cba12cd2cd4431089e09a942fb2ea0f6ca8e33f3
AmirSohailMd <amirsohailmd7@gmail.com>
2026-03-19 22:16:15 +0530

    feat: integrate frontend react files and update server configuration

--- server/server.js
+++ server/server.js
@@ -13,11 +13,45 @@ const messageRoutes = require("./routes/messageRoutes");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server,{
+const io = new Server(server, {
   pingTimeout: 60000,
-  cors:{
+  cors: {
     origin: "*",
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log("User joined room: ", userData._id);
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined chat: ", room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+
+    if (!chat.users) return;
+
+    chat.users.forEach((user) => {
+      if (user == newMessageReceived.sender._id) return;
+
+      socket.in(user).emnit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User Disconnected");
+    socket.leave(userData._id);
+  });
 });
 
 app.use(express.json());
@@ -31,7 +65,11 @@ app.get("/", (req, res) => {
   res.send("API is running..");
 });
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
